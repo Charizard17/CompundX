567b0d2f658bde19abcbb07ae0ae7e8d29bca896
Charizard17 <3723esad@gmail.com>
2025-08-20 13:46:54 +0200

    growth chart created

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'screens/dashboard_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CompoundXApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CompoundXApp extends StatelessWidget {
+  const CompoundXApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CompundX',
-      theme: ThemeData(),
-      home: Center(child: Text("test")),
+      title: 'CompoundX - Crypto Trading Tracker',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.purple,
+        scaffoldBackgroundColor: const Color(0xFF0A0A0A),
+        cardColor: const Color(0xFF1A1A1A),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white70),
+        ),
+      ),
+      home: const DashboardScreen(),
     );
   }
 }
